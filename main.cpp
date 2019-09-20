399a2f5d061ee6808de8e3635327fb4fab89dedf
Kaique Gomes Machado <kaiquered@hotmail.com>
2019-09-20 08:24:33 -0300

    Primeira inserção de arquivo

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <Sculptor.h>
+
+using namespace std;
+
+int main()
+{
+    struct Voxel {
+
+      float r,g,b;
+
+      float a;
+
+      bool isOn;
+    };
+
+
+
+
+
+}
---
+++ sculptor.cpp
@@ -0,0 +1,74 @@
+#include <Sculptor.h>
+
+Sculptor::Sculptor()
+{
+
+
+}
+
+void Sculptor::setColor(float r, float g, float b, float alpha)
+{
+    this -> r = r;
+    this -> g = g;
+    this -> b = b;
+    this -> alpha = alpha;
+
+}
+
+void Sculptor::putVoxel(int x, int y, int z)
+{
+    v[x][y][z].r = r;
+    v[x][y][z].g = g;
+    v[x][y][z].b = b;
+    v[x][y][z].alpha = alpha;
+
+    isOn = true;
+
+}
+
+void Sculptor::cutVoxel(int x, int y, int z)
+{
+    v[x][y][z].r = r;
+    v[x][y][z].g = g;
+    v[x][y][z].b = b;
+    v[x][y][z].alpha = alpha;
+
+    isOn = false;
+
+
+}
+
+void Sculptor::putBox(int x0, int x1, int y0, int y1, int z0, int z1)
+{
+
+}
+
+void Sculptor::cutBox(int x0, int x1, int y0, int y1, int z0, int z1)
+{
+
+}
+
+void Sculptor::putSphere(int xcenter, int ycenter, int zcenter, int radius)
+{
+
+}
+
+void Sculptor::cutSphere(int xcenter, int ycenter, int zcenter, int radius)
+{
+
+}
+
+void Sculptor::putEllipsoid(int xcenter, int ycenter, int zcenter, int rx, int ry, int rz)
+{
+
+}
+
+void Sculptor::cutEllipsoid(int xcenter, int ycenter, int zcenter, int rx, int ry, int rz)
+{
+
+}
+
+void Sculptor::writeOFF(char *filename)
+{
+
+}
