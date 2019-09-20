36d5fc4a873f03d02c7b7b89ed247eeabe00cfd5
Lucy Costa <30415579+lucycosta@users.noreply.github.com>
2019-09-20 13:46:39 -0300

    Estrutura e classe dadas pelo professor

--- Sculptor.h
+++ Sculptor.h
@@ -1,6 +1,13 @@
 #ifndef SCULPTOR_H
 #define SCULPTOR_H
 
+struct Voxel {
+  float r,g,b; // Colors
+  float a;
+// Transparency
+  bool isOn; // Included or not
+};
+
 class Sculptor {
 
 protected:
